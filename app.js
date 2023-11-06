b28cc499c651462f5befc0c0f8c8e54b10404494
Olegs Radcenko <olegs.radcenko@gmail.com>
2023-11-06 08:47:32 +0100

    update

--- app.js
+++ app.js
@@ -1,66 +1,78 @@
-require('dotenv').config(); // Make sure this line is at the top of the file
+require('dotenv').config(); // Load environment variables from .env file
 
 const express = require('express');
-const AWS = require('aws-sdk');
+const { S3Client, PutObjectCommand, GetObjectCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
 const { v4: uuidv4 } = require('uuid');
-
-// Configure AWS with environment variables from .env file
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  sessionToken: process.env.AWS_SESSION_TOKEN, // Include if you have a session token
-  region: process.env.AWS_REGION
-});
-
-// Create an S3 service object
-const s3 = new AWS.S3();
+const stream = require('stream');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const BUCKET_NAME = process.env.CYCLIC_BUCKET_NAME; // Use the Cyclic bucket name from .env
+const BUCKET_NAME = process.env.CYCLIC_BUCKET_NAME;
+
+// Create an S3 client
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    sessionToken: process.env.AWS_SESSION_TOKEN // Include if you have a session token
+  }
+});
 
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-// POST handler to save text to S3
-app.post('/', (req, res) => {
-  const { Content } = req.body;
-  const fileName = uuidv4(); // Create a unique file name
+// In-memory store for demonstration purposes
+let fileMap = {};
 
-  // Parameters for S3 upload
+// POST handler to save text to S3
+app.post('/', async (req, res) => {
+  const { Content } = req.body;
+  const fileName = `file-${uuidv4()}`; // Prefixing the UUID to ensure uniqueness across all files
+
   const params = {
     Bucket: BUCKET_NAME,
     Key: fileName,
     Body: Content
   };
 
-  // Upload text to S3
-  s3.upload(params, (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: "Error uploading data to S3." });
-    }
-    res.json({ message: 'Text saved successfully', fileUrl: data.Location });
-  });
+  try {
+    const command = new PutObjectCommand(params);
+    await s3.send(command);
+    fileMap[fileName] = Content; // Store in in-memory map (replace this with a database in production)
+    res.json({ message: 'Text saved successfully', fileName: fileName });
+  } catch (err) {
+    res.status(500).json({ error: "Error uploading data to S3.", details: err.message });
+  }
 });
 
 // GET handler to retrieve text from S3
-app.get('/', (req, res) => {
-  // For simplicity, the file name is fixed. In a real app, you'd have a way to determine this.
-  const fileName = 'the-name-of-your-file';
+app.get('/', async (req, res) => {
+  const fileNames = Object.keys(fileMap); // Retrieve file names from in-memory store
+  if (!fileNames.length) {
+    return res.status(404).json({ error: "No files saved." });
+  }
 
-  // Parameters to get the file from S3
-  const params = {
-    Bucket: BUCKET_NAME,
-    Key: fileName
-  };
+  try {
+    // Assuming we want the latest file saved
+    const latestFileName = fileNames[fileNames.length - 1];
+    const getParams = {
+      Bucket: BUCKET_NAME,
+      Key: latestFileName
+    };
 
-  // Retrieve text from S3
-  s3.getObject(params, (err, data) => {
-    if (err) {
-      return res.status(500).json({ error: "Error retrieving data from S3." });
-    }
-    res.json({ Content: data.Body.toString() });
-  });
+    const command = new GetObjectCommand(getParams);
+    const { Body } = await s3.send(command);
+    
+    // Stream the S3 object data into a string
+    let dataString = '';
+    Body.on('data', (chunk) => dataString += chunk);
+    Body.on('end', () => {
+      res.json({ fileName: latestFileName, Content: dataString });
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Error retrieving data from S3.", details: err.message });
+  }
 });
 
 // Start the server
@@ -70,20 +82,3 @@ app.listen(PORT, () => {
 
 // Export the app for testing purposes
 module.exports = app;
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-//   res.json({ message: 'Hello World' });
-// });
-
-// app.post('/', (req, res) => {
-
-// })
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-// module.exports = app; // For testing purposes
